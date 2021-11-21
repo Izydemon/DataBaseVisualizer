89010e786cb02505d40d1bd62af5449c56274654
Isidro <izydemon95@gmail.com>
2021-11-21 12:58:26 +0000

    Minor Changes

--- src/main/java/ui/dataBaseView.java
+++ src/main/java/ui/dataBaseView.java
@@ -70,7 +70,7 @@ public class dataBaseView extends javax.swing.JFrame {
         jMenu2 = new javax.swing.JMenu();
         helpButton = new javax.swing.JMenuItem();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
         setResizable(false);
         addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowClosing(java.awt.event.WindowEvent evt) {
